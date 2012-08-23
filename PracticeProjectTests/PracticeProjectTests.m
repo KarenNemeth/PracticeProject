6c359d96f5a3d75c3ebf11a20d8703a5e3409413
806-16 <80616@41cs806-16.cooper.edu>
2012-08-23 10:02:13 -0400

    Initial Commit

---
+++ PracticeProjectTests/PracticeProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  PracticeProjectTests.m
+//  PracticeProjectTests
+//
+//  Created by student on 8/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PracticeProjectTests.h"
+
+@implementation PracticeProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PracticeProjectTests");
+}
+
+@end
